bd27055fc40bcf4ab925b06dd6e3a84f64cc31f8
DevInnothoughts <devsupport@innothoughts.com>
2026-05-26 15:38:43 +0530

    webpack public path updated

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-    publicPath: "/", // ✅ this line must be here
+    publicPath: "/hhc/", // ✅ this line must be here
     clean: true,
   },
   module: {
